46838ca4b2c8fa9897b52c1d17b0e978f5969b0f
ayuka9494 <167833160+ayuka9494@users.noreply.github.com>
2025-01-19 15:43:55 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,6 +1,6 @@
 function imgchange1() {
-    document.getElementById('niziu').src = './image/mako1.jpeg';
+    document.getElementById('totoro').src = './image/mako1.jpeg';
 }
 function imgchange2() {
-    document.getElementById('niziu').src = './image/mako.jpeg';
+    document.getElementById('totoro').src = './image/mako.jpeg';
 }
